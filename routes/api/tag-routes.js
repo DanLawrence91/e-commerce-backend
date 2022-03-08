4bb50fa824219e5f128cd7fffbb8756f5389fe14
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-03-08 16:02:13 +1030

    routes made async so can use try catch on tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,7 +3,7 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   try {
     const data = await Tag.findAll({
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
 
   try {
@@ -32,11 +32,11 @@ router.get("/:id", (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
 });
 
@@ -59,27 +59,6 @@ router.delete("/:id", async (req, res) => {
 });
 
 module.exports = router;
-
-const router = require("express").Router();
-const { Category, Product } = require("../../models");
-
-router.get("/:id", async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-
-  try {
-    const data = await Category.findByPk(req.params.id, {
-      include: [{ model: Product, group: "category_id" }],
-    });
-    if (!data) {
-      res.status(404).json({ message: "No category found with this id!" });
-      return;
-    }
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 router.post("/", async (req, res) => {
   // create a new category
