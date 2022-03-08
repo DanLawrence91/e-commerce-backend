824c1756ada0d764f4a599e87fc83a974e5b49e5
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-03-08 16:08:14 +1030

    product routes made async and delete route added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -79,21 +79,3 @@ router.delete("/:id", async (req, res) => {
 });
 
 module.exports = router;
-
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
-  try {
-    const updateCategory = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!updateCategory) {
-      res.status(404).json({ message: "No category found with this id!" });
-      return;
-    }
-    res.status(200).json(updateCategory);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
